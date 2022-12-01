39c68bdc0f521bee3d8e03ca069253c0ce93f336
zhoomabekov <gzhumabekov@nu.edu.kz>
2022-12-01 19:28:10 +0600

    class Ship is ready

--- Morskoi_Boi_MAIN.py
+++ Morskoi_Boi_MAIN.py
@@ -1,1 +1,14 @@
-print('Hello World')
+class Ship:
+    __init__(self, length, front, direction, life):\
+        self.length = length
+    self.front = front
+    self.direction = direction
+    self.life = life
+
+
+def dots(self):
+    ship_cells = []
+    for i in range(self.length):
+        cell = (self.front[i] + self.direction[i], self.front[i + 1] + self.direction[i + 1])
+        ship_cells.append(cell)
+    return ship_cells
