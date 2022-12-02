ef44f25fa56168c1a795e7da280c37a5f82b786d
zhoomabekov <gzhumabekov@nu.edu.kz>
2022-12-03 02:57:29 +0600

    still Board

--- Internal_logic_classes.py
+++ Internal_logic_classes.py
@@ -62,3 +62,93 @@ class Ship:
             ship_cells.append(cell)
         return ship_cells
 
+
+class Board:
+    POSSIBLE_CELLS = []
+    for i in range(6):
+        for j in range(6):
+            POSSIBLE_CELLS.append((i, j))
+    #     print(POSSIBLE_CELLS)
+
+    player_shots_left = []
+    for i in POSSIBLE_CELLS:
+        player_shots_left.append(Dot(*i))
+    #     print(len(player_shots_left))
+
+    start_cells_conditions = [['o' for i in range(6)] for i in range(6)]
+
+    __s = u'\u220E'  # код квадрата в системе unicode
+
+    def __init__(self, hid, cells_conditions=start_cells_conditions, ships=[], alive_ships=7):
+        self.cells_conditions = cells_conditions  # list
+        self.ships = ships  # list with all ships
+        self.hid = hid  # bool
+        self.alive_ships = alive_ships  # int
+
+    def add_ship(self, ship):  # ставит корабль на доску (если ставить не получается, выбрасываем исключения)
+        for i in range(len(ship.dots())):
+            if ship.dots()[i] in self.contoured_ships():
+                raise ShipLocationError(ship.length, ship.front, ship.direction)
+        self.ships.append(ship.dots())
+        print(f'ships = {self.ships}')
+
+    def contoured_ships(self):  # точки всех кораблей + их контуров
+
+        existing_ships_dots = []
+        for i in range(len(self.ships)):
+            existing_ships_dots += self.ships[i]
+
+        contoured_ships_dots = []
+        for dot in range(len(existing_ships_dots)):
+            for i in [-1, 0, 1]:
+                for j in [-1, 0, 1]:
+                    tested_dot = (existing_ships_dots[dot][0] + i, existing_ships_dots[dot][1] + j)
+                    if tested_dot in self.POSSIBLE_CELLS:
+                        contoured_ships_dots.append(tested_dot)
+        return list(set(contoured_ships_dots))
+
+    def show_board(self):  # выводит доску в консоль в зависимости от параметра hid.
+        ships_gui = self.POSSIBLE_CELLS.copy()
+        existing_ships_dots = []
+        for i in range(len(self.ships)):
+            existing_ships_dots += self.ships[i]
+
+        if not hid:
+            return self.ships
+
+    def out(self, dot):  # для точки (объекта класса Dot) возвращает True, если точка выходит за пределы поля
+        if dot in self.POSSIBLE_CELLS:
+            return False
+        else:
+            return True
+
+    def shot(self):  # делает выстрел по доске (если есть попытка выстрелить за \
+        # пределы и в использованную точку, нужно выбрасывать исключения
+        while True:  # Loop для получения правильного ввода от Игрока
+            try:
+                player_shot = int(input("В какую ячейку будем стрелять?"))
+                row = player_shot // 10 - 1
+                col = player_shot % 10 - 1
+
+                if self.out((row, col)):
+                    raise BoardOutError(player_shot)
+                else:
+                    shot = Dot(row, col)
+
+                    if shot not in self.player_shots_left:
+                        err = NonEmptyError(player_shot)
+                        print(err)
+                    else:
+                        if self.ships[row][col] == 'o':
+                            self.cells_conditions[row][col] = 'T'
+                            self.player_shots_left.remove(Dot(row, col))
+                            print(self.cells_conditions)
+                        else:
+                            self.cells_conditions[row][col] = 'X'
+                            self.player_shots_left.remove(Dot(row, col))
+                            print(self.cells_conditions)
+                        print('Ход компьютера:')
+                        break
+
+            except Exception:
+                print('Необходимо ввести двузначное число!')
